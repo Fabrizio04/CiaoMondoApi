0c1505de99b450caaed8dcf510eba30a7ef7ffc6
Fabrizio Amorelli <amorellif94@gmail.com>
2026-03-02 20:49:43 +0100

    Aggiornata pipeline su master, aggiuti file per img docker e unit test

---
+++ CiaoMondoApi.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace CiaoMondoApi.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestEsempio()
+        {
+            // Un test che passa sempre per verificare la pipeline
+            Assert.True(true);
+        }
+    }
+}
